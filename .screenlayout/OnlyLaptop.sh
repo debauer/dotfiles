d283d6a642647834226f7032ae72ba0fb3d9c5ba
debauer <debauer@users.noreply.github.com>
2019-12-16 11:23:35 +0100

    changed display configs

--- .screenlayout/OnlyLaptop.sh
+++ .screenlayout/OnlyLaptop.sh
@@ -3,4 +3,4 @@ INTERN="--output eDP1 --mode 1920x1080 --pos 1920x1080"
 EXTERN1="--output DP2-2 --off"
 EXTERN2="--output DP2-3 --off"
 EXTERN2="--output HDMI2 --off"
-xrandr $INTERN $EXTERN1 $EXTERN2
+xrandr $INTERN $EXTERN1 $EXTERN2 $EXTERN3
