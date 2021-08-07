95a2056dd7d9b8b07bf387eb44fd05b452adfdb9
David Bauer <bauer@synyx.de>
2021-08-07 18:37:34 +0200

    screenlayouts

--- screenlayout/.screenlayout/OnlyLaptop.sh
+++ screenlayout/.screenlayout/OnlyLaptop.sh
@@ -2,5 +2,5 @@
 INTERN="--output eDP1 --mode 1920x1080 --pos 1920x1080"
 EXTERN1="--output DP2-2 --off"
 EXTERN2="--output DP2-3 --off"
-EXTERN2="--output HDMI2 --off"
+EXTERN3="--output HDMI2 --off"
 xrandr $INTERN $EXTERN1 $EXTERN2 $EXTERN3
